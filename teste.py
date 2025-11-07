38a4b118561de63105c3055d2a83a4d27d6c7132
Almeidiano <samuel10ap@gmail.com>
2025-11-07 18:53:14 -0300

    calculadora com samuel e biel!

--- teste.py
+++ teste.py
@@ -1,1 +1,28 @@
-print("ola sam")
+# operação de adição
+
+# 1. pega no minimo 2 ou mais numeros 
+# 2. faz o calculo
+# 3. mostra
+
+# int
+def adicao(x, y):
+    z = x + y
+    return z
+
+def subtracao(x, y):
+    z = x - y
+    return z
+
+def multiplicaçao(x, y):
+    z = x * y
+    return z
+
+def divisao(x, y):
+    if y == 0:
+        return "Cannot divide by zero"
+
+    z = x / y
+    return z
+
+
+print(divisao(23,0))
